39973768cd65e5042b8c93d3f8b5f895c9d261c8
pk8101 <pradeepvirat913@gmail.com>
2023-02-19 00:23:38 +0530

    title of the app SocialRoute

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -6,29 +6,32 @@ export default function Topbar() {
     <div className="topbarContainer">
       <div className="topbarLeft">
         <div className="logo">
-          letsPingSocial
+          SocialRoute
         </div>
       </div>
       <div className="topbarCenter">
         <div className="searchBar">
           <Search className="searchIcon"/>
-          <input placeholder="Search or friends ,Posts" className="searchInput"/>
+          <input placeholder="Search for friends, Posts" className="searchInput"/>
         </div>
       </div>
       <div className="topbarRight">
         <div className="topbarLinks">
-          <span className="topbarLInk">Homepage</span>
-          <span className="topbarLInk">TimeLine</span>
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">TimeLine</span>
         </div>
         <div className="topbarIcons">
           <div className="topbaricon">
             <Person/>
+            <span className="topbarIconBadges">1</span>
           </div>
           <div className="topbaricon">
             <Chat/>
+            <span className="topbarIconBadges">4</span>
             </div>
           <div className="topbaricon">
             <Notifications/>
+            <span className="topbarIconBadges">3</span>
           </div>
         </div>
         <img src="./logos/luffy.jpg" alt="" className="topbarImg" />
