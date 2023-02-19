a52340f1326dde7154d9943c1eee00aee17f4145
pk8101 <pradeepvirat913@gmail.com>
2023-02-20 00:07:39 +0530

    share component created and added in feed component

---
+++ src/components/share/Share.jsx
@@ -0,0 +1,15 @@
+import './share.css';
+
+export default function Share() {
+  return (
+    <div className='share'>
+        <div className="shareWrapper">
+          <div className="shareTop">
+            <img src="./logos/luffy.jpg" alt="" className="shareImg" />
+          </div>
+          <div className="shareBottom"></div>
+        </div>
+        
+    </div>
+  )
+}
