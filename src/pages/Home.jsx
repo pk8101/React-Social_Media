0e1367d14bbc972890562dbf0b13797c82ce71e9
pk8101 <pradeepvirat913@gmail.com>
2023-02-19 20:05:14 +0530

    feed and rightbar components added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,17 @@
-import './Home.css'
-import Topbar from '../components/topbar/Topbar'
-import Sidebar from '../components/sidebar/Sidebar'
+import './Home.css';
+import Topbar from '../components/topbar/Topbar';
+import Sidebar from '../components/sidebar/Sidebar';
+import Feed from '../components/feed/Feed';
+import Rightbar from '../components/rightbar/Rightbar';
+
 export default function Home() {
   return (
     <>
       <Topbar/>
       <div className="bodyContainer">
         <Sidebar/>
+        <Feed/>
+        <Rightbar/>
       </div>
       
     </>
