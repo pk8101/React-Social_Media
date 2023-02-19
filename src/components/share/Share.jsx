fa1a0c0bad3ac58cd5fa97325897da71d5cf1638
pk8101 <pradeepvirat913@gmail.com>
2023-02-20 01:32:16 +0530

    share component in designed with css

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,4 +1,5 @@
 import './share.css';
+import {PermMedia,EmojiEmotions,LocationOn,Label} from "@mui/icons-material"
 
 export default function Share() {
   return (
@@ -6,8 +7,30 @@ export default function Share() {
         <div className="shareWrapper">
           <div className="shareTop">
             <img src="./logos/luffy.jpg" alt="" className="shareImg" />
+            <input type="text" placeholder="What's in your mind ?"className="shareInput" />
           </div>
-          <div className="shareBottom"></div>
+          <hr className="shareHr" />
+          <div className="shareBottom">
+            <div className="shareOptions">
+              <div className="shareoption">
+                <PermMedia htmlColor='blue'className='shareIcon'/>
+                <span className="shareOptionText">Photo or Video</span>
+              </div>
+              <div className="shareoption">
+                <Label htmlColor='black' className='shareIcon'/>
+                <span className="shareOptionText">Tag</span>
+              </div>
+              <div className="shareoption">
+                <EmojiEmotions htmlColor='grey' className='shareIcon'/>
+                <span className="shareOptionText">Reaction</span>
+              </div>
+              <div className="shareoption">
+                <LocationOn htmlColor='red' className='shareIcon'/>
+                <span className="shareOptionText">Location</span>
+              </div>
+            </div>
+            <button className="shareButton">Share</button>
+          </div>
         </div>
         
     </div>
