25c03caeb4ab35af8282b82690c936f5782837ff
pk8101 <pradeepvirat913@gmail.com>
2023-02-20 18:10:22 +0530

    rightbar component designed and added

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -2,6 +2,55 @@ import "./rightbar.css"
 
 export default function rightbar() {
   return (
-    <div className="rightbar">hi i'm rightbar</div>
+    <div className="rightbar">
+      <div className="rightbarwrapper">
+        <div className="birthdaycontainer">
+          <img src="logos/birthday.avif" alt="" className="birthdayimg" />
+          <span className="birthdaytext">
+            <b>Pola Foaster</b> and <b>3 Others Friends</b> have a birthdat today
+          </span>
+        </div>
+        <img src="logos/birthday1.jpg" alt="" className="rightad" />
+        <h4 className="rightbartitle">Online friends</h4>
+        <ul className="rightbarfriendlist">
+          <li className="rigthbarfriend">
+            <div className="rightbarprofileimgcontainer">
+              <img src="logos/madara.jpg" alt="" className="rightbarprofileimg" />
+              <span className="rightbaronline"></span>
+            </div>
+            <span className="rightbarusername">Madara</span>
+          </li>
+          <li className="rigthbarfriend">
+            <div className="rightbarprofileimgcontainer">
+              <img src="logos/naruto.jpg" alt="" className="rightbarprofileimg" />
+              <span className="rightbaronline"></span>
+            </div>
+            <span className="rightbarusername">naruto</span>
+          </li>
+          <li className="rigthbarfriend">
+            <div className="rightbarprofileimgcontainer">
+              <img src="logos/sasukea.jpeg" alt="" className="rightbarprofileimg" />
+              <span className="rightbaronline"></span>
+            </div>
+            <span className="rightbarusername">sasukea</span>
+          </li>
+          <li className="rigthbarfriend">
+            <div className="rightbarprofileimgcontainer">
+              <img src="logos/uchiha.jpg" alt="" className="rightbarprofileimg" />
+              <span className="rightbaronline"></span>
+            </div>
+            <span className="rightbarusername">itachi</span>
+          </li>
+          <li className="rigthbarfriend">
+            <div className="rightbarprofileimgcontainer">
+              <img src="logos/madara.jpg" alt="" className="rightbarprofileimg" />
+              <span className="rightbaronline"></span>
+            </div>
+            <span className="rightbarusername">Zoro</span>
+          </li>
+        </ul>
+
+      </div>
+    </div>
   )
 }
